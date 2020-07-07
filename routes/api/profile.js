d6c52921bd828d0e429bacf71d742f7998560af7
Daniel Kang <dkang@gennet.com>
2020-07-06 22:53:25 -0700

    Get all profiles and profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -11,7 +11,9 @@ const User = require('../../models/User');
 // @access  Private
 router.get('/me', auth, async (req, res) => {
     try {
-        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+        const profile = await Profile.findOne({
+            user: req.user.id,
+        }).populate('user', ['name', 'avatar']);
 
         if (!profile) {
             return res.status(400).json({ msg: 'There is no profile for this user' });
@@ -27,10 +29,15 @@ router.get('/me', auth, async (req, res) => {
 // @route   POST api/profile
 // @desc    Create or update user profile
 // @access  Private
-router.post('/', [auth, [
-    check('status', 'Status is required').not().isEmpty(),
-    check('skills', 'Skills are required').not().isEmpty()
-]],
+router.post(
+    '/',
+    [
+        auth,
+        [
+            check('status', 'Status is required').not().isEmpty(),
+            check('skills', 'Skills are required').not().isEmpty(),
+        ],
+    ],
     async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -49,7 +56,7 @@ router.post('/', [auth, [
             facebook,
             twitter,
             instagram,
-            linkedin
+            linkedin,
         } = req.body;
 
         // Build profile object
@@ -62,7 +69,7 @@ router.post('/', [auth, [
         if (status) profileFields.status = status;
         if (githubusername) profileFields.githubusername = githubusername;
         if (skills) {
-            profileFields.skills = skills.split(',').map(skill => skill.trim());
+            profileFields.skills = skills.split(',').map((skill) => skill.trim());
         }
 
         // Build social object
@@ -73,16 +80,19 @@ router.post('/', [auth, [
         if (linkedin) profileFields.social.linkedin = linkedin;
         if (instagram) profileFields.social.instagram = instagram;
 
-
         try {
             let profile = await Profile.findOne({ user: req.user.id });
 
             if (profile) {
                 // Update
-                profile = await Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true });
+                profile = await Profile.findOneAndUpdate(
+                    { user: req.user.id },
+                    { $set: profileFields },
+                    { new: true }
+                );
 
                 return res.json(profile);
-            };
+            }
 
             // Create
             profile = new Profile(profileFields);
@@ -93,6 +103,38 @@ router.post('/', [auth, [
             console.error(err.message);
             res.status(500).send('Server Error');
         }
-    })
+    }
+);
+
+// @route   GET api/profile
+// @desc    get all profiles
+// @access  Public
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    get profile by user ID
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar'])
+
+        if (!profile) return res.status(400).json({ msg: 'Profile not found' })
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind == 'ObjectId') {
+            return res.status(400).json({ msg: 'Profile not found' })
+        }
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
